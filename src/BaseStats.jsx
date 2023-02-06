0bbe77439d2b9e6a3d577d1ebe78473f90c38b3d
Amalya Megerman <amalyamegerman@gmail.com>
2023-02-06 13:51:06 -0500

    add onclick

--- src/BaseStats.jsx
+++ src/BaseStats.jsx
@@ -1,9 +1,32 @@
 import "./BaseStats.css";
 
-function BaseStats() {
+function BaseStats({ hp, attack, defense, speed, clicker }) {
   return (
     <div className="base-stats">
       <h1>BaseStats</h1>
+      <button class="sp-stats" onclick={clicker}>
+        Check Special Stats
+      </button>
+      <table>
+        <tbody>
+          <tr>
+            <td>Hit Points</td>
+            <td>{hp}</td>
+          </tr>
+          <tr>
+            <td>Attack</td>
+            <td>{attack}</td>
+          </tr>
+          <tr>
+            <td>Defense</td>
+            <td>{defense}</td>
+          </tr>
+          <tr>
+            <td>Speed</td>
+            <td>{speed}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
